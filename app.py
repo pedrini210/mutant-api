4245d5d93d0e1b01ca6bae870b4cfd5cd5f16e19
Pedro Castillo <prcastillo.d@gmail.com>
2018-05-22 17:40:38 -0300

    NxN Inputs

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import boto3
 app = Flask(__name__)
 
 dynamodb_table_name = 'mutant-api'
-dynamodb = boto3.resource('dynamodb')
-dynamo_table = dynamodb.Table(dynamodb_table_name)
-
 
 @app.route('/')
 def index():
@@ -18,6 +15,8 @@ def index():
 # Challenge Level 2
 @app.route('/mutant/', methods=['POST'])
 def validate_mutant():
+    dynamodb = boto3.resource('dynamodb')
+    dynamo_table = dynamodb.Table(dynamodb_table_name)
     if not request.json or 'dna' not in request.json: return abort(403)
     dna = request.json['dna']
     try:
@@ -36,18 +35,29 @@ def validate_mutant():
 # Challenge Level 3
 @app.route('/stats', methods=['GET'])
 def mutant_stats():
-    mutant_stats_dict = {
-        "count_mutant_dna": 0,
-        "count_human_dna": 0,
-        "ratio": 0.0
+    client = boto3.client('dynamodb')
+    paginator = client.get_paginator('scan')
+    operation_parameters = {
+        'TableName': dynamodb_table_name,
+        'FilterExpression': 'mutant = :x',
+        'ExpressionAttributeValues': {
+        ':x': {'BOOL': True},
+        }
     }
-    query_dynamo = dynamo_table.scan()
-    population = query_dynamo['Count']
+    mutant_stats_dict = {
+            "count_mutant_dna": 0,
+            "count_human_dna": 0,
+            "ratio": 0.0
+        }
+    population = 0
+    mutants_qty = 0
+    page_iterator = paginator.paginate(**operation_parameters)
+    for page in page_iterator:
+        population += page['ScannedCount']
+        mutants_qty += page['Count']
     if population == 0:
         return jsonify(mutant_stats_dict)
     else:
-        items = query_dynamo['Items']
-        mutants_qty = len([item for item in items if item['mutant'] is True])
         humans_qty = population - mutants_qty
         mutant_stats_dict['count_mutant_dna'] = mutants_qty
         if humans_qty != 0:
